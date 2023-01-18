d6b7846374f585b70e79705d79388d88204b322f
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2023-01-18 15:53:17 +0100

    Installed Sass and created base components

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'AppHeader',
+}
+
+</script>
+
+<template>
+    <header>
+        <h1>Header</h1>
+    </header>
+</template>
+
+<style lang="scss">
+header {
+    display: flex;
+    justify-content: center;
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'AppMain',
+}
+
+</script>
+
+<template>
+    <main>
+        <h1>Main</h1>
+    </main>
+</template>
+
+<style lang="scss">
+main {
+    display: flex;
+    justify-content: center;
+}
+</style>
